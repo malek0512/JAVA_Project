724e88d829b4cc409bd59cbacfce100fae0164f0
arame <arame@ubuntu.(none)>
2014-04-24 07:19:06 -0700

    Circuit Arame

--- src/circuit/$Ferme.java
+++ src/circuit/$Ferme.java
@@ -1,4 +1,9 @@
 package circuit;
+
+import java.util.LinkedList;
+import java.util.List;
+
+import composant._Composant;
 /*
  	--Invariant--
 Aucune connexion est libre (entrer ou sortie)
@@ -10,5 +15,11 @@ Aucune connexion est libre (entrer ou sortie)
  */
 
 public abstract class $Ferme implements _Circuit {
-
+	protected List<_Composant> Listcomposant;
+	public $Ferme(){
+		Listcomposant = new LinkedList<_Composant>();  
+	}
+	
+	
+	
 }
--- src/circuit/Composite.java
+++ src/circuit/Composite.java
@@ -1,7 +1,71 @@
 package circuit;
 
+import java.util.ArrayList;
+
+import port.In;
+import port.Out;
 import composant._Composant;
 
 public class Composite extends $Ouvert implements _Composant {
+	
+	private ArrayList<In> ArrayEntree;
+	private ArrayList<Out> ArraySortie;
+
+	/**
+	 * Permet de creer un composite
+	 * @param nbEntree : Nombre d'Entree du composite
+	 * @param nbSortie : Nombre de Sortie du composite
+	 */
+	public Composite (int nbEntree, int nbSortie){
+		super();
+		ArrayEntree = new ArrayList<In>();
+		ArraySortie = new ArrayList<Out>();
+		for (int i = 0; i < nbEntree; i++) {
+				ArrayEntree.add(new In());
+		}
+		for (int j = 0; j < nbSortie; j++) {
+			ArraySortie.add(new Out());
+		}
+	}
+	
+	 /* @return Retourne le nombre de composant */
+			public int nbComposant(ArrayList<_Composant> ArrayComposant){
+				return ArrayComposant.size();
+			}
+			
+	/* @return le nombre de sorties d'un composite */
+			public int nbsortie() {
+				return ArraySortie.size();
+			}
+	/*@return le nombre d'entrees d'un composite*/
+			public int nbentree(){
+				return ArrayEntree.size();
+				
+			}
+
+			@Override
+			public void nbPortEntreeLibre() {
+				// TODO Auto-generated method stub
+				
+			}
+
+			@Override
+			public void nbPortSortieLibre() {
+				// TODO Auto-generated method stub
+				
+			}
+
+			@Override
+			public void ajoutComposant() {
+				// TODO Auto-generated method stub
+				
+			}
+
+	
+			
+
+	
+
+
 
 }
