7b3d21b2f33d60d39458eeab70c93a8e7c01576e
Christophe Adam <christophe@ubuntu.(none)>
2014-04-24 15:00:20 +0200

    Composant christophe

--- src/composant/$Generateur.java
+++ src/composant/$Generateur.java
@@ -1,4 +1,10 @@
 package composant;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import port.Out;
+
 /*
 juste a implementer l'interface
 et ajouter le constructeur correctement
@@ -10,6 +16,55 @@ n'a aucun port d'entre
 
 */
 public abstract class $Generateur implements _Composant {
-
 	
+	  String Nom;
+	  int idComposant;
+	  private List<Out> Ports_Sortie;
+	
+	  /**
+	   * Constructeur d'un generateur
+	   * @ensure les ports du composant ne seront pas connectés
+	   * @param nom nom du composant à creer
+	   * @param idComposant identifiant du composant à creer
+	   * @param nbSort nombre de sorties du composant
+	   * 
+	   */
+	  public $Generateur(String nom, int idComposant, int nbSort){
+		  this.Nom=nom;
+		  this.idComposant = idComposant;
+		  Ports_Sortie = new ArrayList<Out>();
+	  }
+	
+	  /**
+	   * identifiant du composant
+	   */
+	  public int IdComposant(){
+		  return idComposant;
+	    }
+	  
+	  /**
+	   * revoie le nombre de sortie(s) du composant
+	   */  
+	  public int nbSorties(){
+	    	return Ports_Sortie.size();
+	  }
+
+	  /**
+	   * vérifie si chaque ports du composant élémentaire est connecté
+	   */
+	  public boolean estExecutable(){
+		  
+		  int j=0;
+		  
+		  while(j<=nbSorties() && Ports_Sortie.get(j).getValide()==true)
+		  {
+			  j++ ;
+		  }
+		  
+		  return (j>=Ports_Sortie.size());
+	  }
+	  
+	  public List<Out> out(){
+		  return Ports_Sortie;
+	  }
 }
--- src/composant/$Transformateur.java
+++ src/composant/$Transformateur.java
@@ -1,8 +1,85 @@
 package composant;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import port.In;
+import port.Out;
+
 /*
  au moins un port dans chaque liste
  */
 
 public abstract class $Transformateur implements _Composant {
+	
+	  String Nom;
+	  int idComposant;
+	  private List<In> Ports_Entre;
+	  private List<Out> Ports_Sortie;
+	
+	  /**
+	   * Constructeur d'un transformateur
+	   * @ensure les ports du composant ne seront pas connectés
+	   * @param nom nom du composant à creer
+	   * @param idComposant identifiant du composant à creer
+	   * @param nbEnt nombre d'entrees du composant
+	   * @param nbSort nombre de sorties du composant
+	   * 
+	   */
+	  public $Transformateur(String nom, int idComposant, int nbEnt, int nbSort){
+		  this.Nom=nom;
+		  this.idComposant = idComposant;
+		  Ports_Entre = new ArrayList<In>();
+		  Ports_Sortie = new ArrayList<Out>();
+	  }
+	
+	  /**
+	   * identifiant du composant
+	   */
+	  public int IdComposant(){
+		  return idComposant;
+	    }
+	
+	  /**
+	   * revoie le nombre d'entree(s) du composant
+	   */
+	  public int nbEntrees(){
+	    	return Ports_Entre.size();
+	  }
+	    
+	  /**
+	   * revoie le nombre de sortie(s) du composant
+	   */  
+	  public int nbSorties(){
+	    	return Ports_Sortie.size();
+	  }
 
+	  /**
+	   * vérifie si chaque ports du composant élémentaire est connecté
+	   */
+	  public boolean estExecutable(){
+		  
+		  int i=0;
+		  int j=0;
+		  
+		  while(i<Ports_Entre.size() && Ports_Entre.get(i).getValide()==true)
+		  {
+			  i++ ;
+		  } 	
+		  
+		  while(j<=nbSorties() && Ports_Sortie.get(i).getValide()==true)
+		  {
+			  j++ ;
+		  }
+		  
+		  return (i>=Ports_Entre.size() && j>=Ports_Sortie.size());
+	  }
+	  
+	  public List<In> in(){
+		  return Ports_Entre;
+	  }
+	  
+	  public List<Out> out(){
+		  return Ports_Sortie;
+	  }
 }
